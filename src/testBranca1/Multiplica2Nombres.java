ad924aa5f79bb4fedcb69a28ae189b6605d28e70
SergiGarcia <sergi8866@hotmail.com>
2023-05-26 19:28:04 +0200

    Package: testBranca1
    class Multiplica2Nombres.java & Suma2Nombres.java
    
    new functions added

--- src/testBranca1/Multiplica2Nombres.java
+++ src/testBranca1/Multiplica2Nombres.java
@@ -1,4 +1,8 @@
 package testBranca1;
 
 public class Multiplica2Nombres {
+
+    private int x;
+    private int y;
+
 }
--- src/testBranca1/Suma2Nombres.java
+++ src/testBranca1/Suma2Nombres.java
@@ -1,4 +1,16 @@
 package testBranca1;
 
 public class Suma2Nombres {
+
+    private int x=2;
+    private int y=3;
+    private int res;
+
+    public int Suma(){
+
+        res=x+y;
+
+        return res;
+    }
+
 }
