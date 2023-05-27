594f17a29a9fd48412b3a2ff91f532f9002cc93b
SergiGarcia <sergi8866@hotmail.com>
2023-05-27 16:03:03 +0200

    added new method multiplicar()

--- src/testBranca1/Multiplica2Nombres.java
+++ src/testBranca1/Multiplica2Nombres.java
@@ -5,4 +5,13 @@ public class Multiplica2Nombres {
     private int x;
     private int y;
 
+    public void multiplicar(){
+
+        x=5;
+        y=4;
+        int r=x*y;
+        System.out.print("resultat= "+r);
+
+    }
+
 }
